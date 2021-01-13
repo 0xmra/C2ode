1a1ec5363aaf7eb06bdb1067b6b3207de769efe4
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-13 05:20:43 +0000

    Updated line of code

--- selfcode/errors_looping3.cpp
+++ selfcode/errors_looping3.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main(void){
+int main (void){
   int i;
-  for (i = 0; , i < 3; i++) {
-    printf("Hello ")
+  for (i = 0; i < 3; i++) {
+    printf("Hello ");
     printf("world\n");
   }
   return 0;
