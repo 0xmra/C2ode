ac4a12f564119ba05eb0ce3e77419f686ec7b78d
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-13 05:36:59 +0000

    comments to the code

--- selfcode/find-and-fixErrors.cpp
+++ selfcode/find-and-fixErrors.cpp
@@ -8,12 +8,12 @@ int main (void) {
   printf("-");
   printf("+\n");
 
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < 3; i++) { // "{}" these were missing
       printf("| o | X | o | X | o | X |");
       printf("\n");
       printf("| X | o | X | o | X | o |");
       printf("\n");
-    }
+    } // causing an print error 
 
   printf("+");
   for (i = 0; i <23; i++)
