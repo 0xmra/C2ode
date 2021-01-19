17d735dd27c12bf9da78062eb20841c5ad0c49a6
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-19 02:43:39 +0000

    Pata nhi kya bakwas code kia hai

---
+++ selfcode/procrastinator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int X, Y;
+  printf("Dear Procrastinator,\n");
+  printf("You still have to wait for %d days %d minutes before you can procrastinate!", 25-23, 60*24*X=Y);
+  return 0;
+}
