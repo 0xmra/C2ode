410e5d3f470e68cf018857966a3a64fd57411aba
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-21 23:00:54 +0000

    my age added lol

--- selfcode/storeVariables.c
+++ selfcode/storeVariables.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void) {
-  printf("I am %d.\n",19 );
-  printf("In %d years, I will be %d years old.\n", 8, 19+8);
-  printf("%d years ago, I was %d years old.\n", 11, 19-11);
+  //Creat a variable to store an integer value
+  int age;
+  //Assigne a value to that variable
+  age = 29;
+  printf("I am %d.\n", age);
+  printf("In %d years, I will be %d years old.\n", 8, age+8);
+  printf("%d years ago, I was %d years old.\n", 11, age-11);
   return 0;
 }
