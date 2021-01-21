32e82fb89215ca80d53b3ee1c5aa9821e4e45954
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-21 22:53:56 +0000

    still editing pls wait

---
+++ selfcode/storeVariables.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+  printf("I am %d.\n",19 );
+  printf("In %d years, I will be %d years old.\n", 8, 19+8);
+  printf("%d years ago, I was %d years old.\n", 11, 19-11);
+  return 0;
+}
