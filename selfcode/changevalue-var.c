b52d548752e248d1ecc18f3ae653a804ca548778
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-21 23:29:45 +0000

    Updateed code line of code

---
+++ selfcode/changevalue-var.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+  int balance;
+  balance = 50;
+  printf("I have %d dollars are in my account\n",balance);
+  balance = balance - 40;
+  printf("I have %d dollars in my account\n", balance);
+  return 0;
+}
--- selfcode/procrastinator.c
+++ selfcode/procrastinator.c
@@ -2,6 +2,8 @@
 int main()
 {
     int X, Y;
+    X = 25-23;
+    Y = X;
   printf("Dear Procrastinator,\n");
   printf("You still have to wait for %d days %d minutes before you can procrastinate!", 25-23, 60*24*X=Y);
   return 0;
