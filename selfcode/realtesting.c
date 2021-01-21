5df5e81cbe273c23d2ae3af3e66e547b3963e9ad
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-21 23:49:10 +0000

    Unexpected coding error!!!

---
+++ selfcode/realtesting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int pollution = 0;
+  pollution = 34;
+  pollution = pollution + 34;
+
+  pollution = pollution - 8;
+  pollution = pollution - 20;
+
+  pollution = 10;
+  pollution = pollution + 12;
+  printf(" pollution level equals to %d \n", pollution);
+  return 0;
+}
