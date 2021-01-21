1e30598980725ebd47502f5f5bb4349e97ec53eb
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-21 23:33:14 +0000

    Testing variables and there storage

--- selfcode/changevalue-var.c
+++ selfcode/changevalue-var.c
@@ -5,5 +5,9 @@ int main () {
   printf("I have %d dollars are in my account\n",balance);
   balance = balance - 40;
   printf("I have %d dollars in my account\n", balance);
+  balance = balance - 30;
+  printf("I have %d dollars in my account\n", balance);
+  balance = balance + 50;
+  printf("I have %d dollars in my account\n", balance);
   return 0;
 }
