e628f469129ad44d35ae3fe213e4e3f8e4f01064
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-23 00:04:54 +0000

    Declare and initialize variables in 1-step

---
+++ selfcode/Dec&int-var_step1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int balance;//creating a variable containing integer values
+    balance = 50;//assigning the value 50 in the balance variable
+    printf("I have %d dollars in my account\n", balance);
+    return 0;
+}
