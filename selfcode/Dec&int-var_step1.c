faefe0005a55a3d2d893f9abaa6af029266deea1
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-24 00:33:09 +0000

    nEW Commit comment

--- selfcode/Dec&int-var_step1.c
+++ selfcode/Dec&int-var_step1.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 int main() {
-    int balance;//creating a variable containing integer values
-    balance = 50;//assigning the value 50 in the balance variable
+  // how to get/set good name for your variables
+    int balance;
+    balance = 50;
     printf("I have %d dollars in my account\n", balance);
     return 0;
 }
+
+
+/*                ----NOTE----
+Variable names can use:  
+lowercase and uppercase letters (characters) and digits
+do not use special characters like @ # & " ...
+do not use accented characters like é è à ...
+do not start with a digit
+start only with a letter
+spaces are forbidden
+ _ may be used instead of a space in the name of the variable
+YouCanUseUppercaseLettersBetweenWordsInsteadOfSpaces   */
