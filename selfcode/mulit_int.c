7a332db69758997219e9502430592c709a884421
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-24 04:54:35 +0000

    Read multiple integers using one scanf() statement ----- part 2 added comma

--- selfcode/mulit_int.c
+++ selfcode/mulit_int.c
@@ -2,7 +2,7 @@
 int main(void) {
   int first, second, third;
   printf("Please enter 3 intergers: \n");
-  scanf("%d%d%d", &first, &second, &third);
+  scanf("%d,%d,%d", &first, &second, &third);
   printf("You entered %d for the first, %d for the second, %d for third\n", first, second, third);
   return 0;
 }
