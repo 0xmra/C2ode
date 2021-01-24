8c9d92bf9f0db8b7b1831c1f633891a691baa832
MR-A0101 <harshsinhawoohp@gmail.com>
2021-01-24 05:17:04 +0000

    multi table test FAILED

---
+++ selfcode/mulit-tables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  int i;
+  int io = 0;
+  int num = 0;
+
+  for (i = 0; i < 11 ; i++) {
+    scanf("%d\n", io);
+    printf("%d x io = %d \n", num, io);
+    num = num +1;
+    //io = io *num;
+  }
+  return 0;
+}
