56e43337378a55e119036eac2b0346a040483e38
MR-A <52797899+MR-A0101@users.noreply.github.com>
2021-05-14 22:50:16 +0530

    special char test rebound

--- 0mr-a/MRA.c
+++ 0mr-a/MRA.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
 printf(" __  __ ____         _     \n");
-printf("|  \/  |  _ \       / \    \n");
+printf("|  \\/\  |  _ \\\       / \\\    \n");
 printf("| |\/| | |_) |____ / _ \   \n");
 printf("| |  | |  _ \_____/ ___ \  \n");
 printf("|_|  |_|_| \_\   /_/   \_\ \n");
