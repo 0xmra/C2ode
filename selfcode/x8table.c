25b5275b102db478f2866735d6e6578275b4c6c5
MR-A <52797899+MR-A0101@users.noreply.github.com>
2021-06-16 02:56:33 +0530

    re code

--- selfcode/x8table.c
+++ selfcode/x8table.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(void) {
-  int i;
+
   int io = 0;
   int num = 0;
   int mul; // this will make sure that it uses any number to print the tables.
   
   scanf("%d", &mul);
 
-  for (i = 0; i < 11 ; i++) {
+  for (int i = 0; i < 11 ; i++) {
     printf("%dx%d = %d \n", num, mul, io );
     num = num +1;
     io = io + mul;
