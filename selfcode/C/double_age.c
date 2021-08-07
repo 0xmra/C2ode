c9daf16c5f5daf9af4078f84dbfe14df7d616292
MR-A0101 <harshsinhawoohp@gmail.com>
2021-08-07 21:44:07 +0530

    re entry

--- selfcode/C/double_age.c
+++ selfcode/C/double_age.c
@@ -3,10 +3,8 @@ int main(void){
   int age;
   double height;
 
-  printf("What is your age?");
-  scanf("%d",&age);
-  printf("What is your height");
-  scanf("%lf",&height);
+  printf("What is your age and height?(seprated with spaces)");
+  scanf("%d %lf",&age, &height);
   printf("You are %d years old and %.2lf meters tall.",age, height);
 
   return 0;
